964dc38cc0404b8e2bf5325ab8e238ab8f721c61
JJuhey <dallicjh@gmail.com>
2020-10-10 10:39:56 +0900

    Transfrom NavBar to TS

--- frontend/src/components/MainPage/MainPage.tsx
+++ frontend/src/components/MainPage/MainPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Header } from 'semantic-ui-react'
+// import { Header } from 'semantic-ui-react'
 import dog from '../../assets/cutedog.jpeg'
 
 const MainPage: React.FC = () => {
